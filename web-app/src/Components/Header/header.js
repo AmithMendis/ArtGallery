217ab47f6050139cc26fa74baa1bb95ea24220d8
AmithMendis <amith.mendis@gmail.com>
2025-09-01 15:29:41 +0530

    refactor header component and remove jquery

--- web-app/src/Components/Header/header.js
+++ web-app/src/Components/Header/header.js
@@ -1,89 +1,63 @@
-import React, { Component } from "react";
-import $ from 'jquery';
+import React, { useState, useEffect } from "react";
 import "./header.css";
 
+const Header = () => {
+  const [menuItems, setMenuItems] = useState([
+    { Title: "HOME", RouteURLPart: "#", ClassName: "selected" },
+    { Title: "SHOP", RouteURLPart: "#", ClassName: "not-selected" },
+    { Title: "LEARN", RouteURLPart: "#", ClassName: "not-selected" },
+    { Title: "ARTISTS", RouteURLPart: "#", ClassName: "not-selected" }
+  ]);
 
-class Header extends Component {
-  state = {
-    menuItems: [
-      {
-        Title: "HOME",RouteURLPart: "#",ClassName: "selected",
-      },
-      {
-        Title: "SHOP",RouteURLPart: "#",ClassName: "not-selected",
-      },
-      {
-        Title: "LEARN",RouteURLPart: "#",ClassName: "not-selected",
-      },
-      {
-        Title: "ARTISTS",RouteURLPart: "#",ClassName: "not-selected",
-      }
-    ]
+  const selectMenuItem = (menuItemName) => {
+    setMenuItems((items) =>
+      items.map((item) =>
+        item.Title === menuItemName
+          ? { ...item, ClassName: "selected" }
+          : { ...item, ClassName: "not-selected" }
+      )
+    );
   };
 
-  selectMenuItem(menuItemName) {
-    var items = this.state.menuItems;
-    for (var i = 0; i < items.length; i++) {
-      if(items[i].Title == menuItemName){
-        items[i].ClassName = "selected";
-      }else{
-        items[i].ClassName = "not-selected";
+  useEffect(() => {
+    const showHeaderAt = 150;
+    const handleScroll = () => {
+      if (window.scrollY > showHeaderAt) {
+        document.body.classList.add("fixed");
+      } else {
+        document.body.classList.remove("fixed");
       }
-    }
-    
-    this.setState({
-      menuItems:items  
-    })
-  }
+    };
 
-  render() {
-    return (
-      <header className="header-fixed">
-        <div className="header-limiter">
-          <h1>
-            <a href="#">
-              Art<span>Gallery</span>
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className="header-fixed">
+      <div className="header-limiter">
+        <h1>
+          <a href="#">
+            Art<span>Gallery</span>
+          </a>
+        </h1>
+        <nav>
+          {menuItems.map((item) => (
+            <a
+              key={item.Title}
+              href={item.RouteURLPart}
+              className={item.ClassName}
+              onClick={() => selectMenuItem(item.Title)}
+            >
+              {item.Title}
             </a>
-          </h1>
-          <nav>
-            {this.state.menuItems.map((item) => (
-              <a
-                key={item.Title}
-                href={item.RouteURLPart}
-                className={item.ClassName}
-                onClick={() => this.selectMenuItem(item.Title)}
-              >
-                {item.Title}
-              </a>
-            ))}
-          </nav>
-        </div>
-      </header>
-    );
-  }
-
-  componentDidMount() {
-    $(document).ready(function () {
-      var showHeaderAt = 150;
-
-      var win = $(window),
-        body = $("body");
-
-      // Show the fixed header only on larger screen devices
-      if (win.width() > 600) {
-        // When we scroll more than 150px down, we set the
-        // "fixed" class on the body element.
-
-        win.on("scroll", function (e) {
-          if (win.scrollTop() > showHeaderAt) {
-            body.addClass("fixed");
-          } else {
-            body.removeClass("fixed");
-          }
-        });
-      }
-    });
-  }
-}
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
